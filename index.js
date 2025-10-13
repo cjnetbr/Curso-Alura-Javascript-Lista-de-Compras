ee1efcd4e5a98f8e39d453d3eb7b6be893708d4e
Claudio <cjnetbr@gmail.com>
2025-10-13 18:50:54 -0300

    Criando Interações -  Continuação

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
-function addToList() {
-    const getItem = document.getElementById("input-item").value.trim();
+const inputItem = document.getElementById("input-item");
 
-    if(getItem === ""){
-        alert("Por favor digite um item para adicionar a lista")
+const botaoSalvar = document.getElementById("adicionar-item");
+
+let contador = 0;
+
+
+botaoSalvar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    //verifica se compo esta vazio
+    if(inputItem.value === ""){
+       alert("Por favor digite um item para adicionar a lista");
+       return;
     }
 
+    //criar os elementas da lista
     const li = document.createElement("li");
 
     const div = document.createElement("div");
     div.classList.add("lista-item-container");
-    
+        
     const input = document.createElement("input");
     input.type = "checkbox";
-    input.id = "checkbox-1";
+    input.id = "checkbox-" + contador++;
     
+        
     const pItem = document.createElement("p");
     pItem.classList.add("texto-item");
-    pItem.textContent = getItem;
-    
+    pItem.textContent = inputItem.value;
+        
     //const pData = document.createElement("p");
-    
+        
     div.appendChild(input);
     div.appendChild(pItem);
     li.appendChild(div);
-    document.getElementById("lista-de-compras").appendChild(li)
+
+
+    const addToList = document.getElementById("lista-de-compras");
+    addToList.appendChild(li);
+
+
+    const limparInput = document.getElementById("input-item").value = "";
 
    
-    
-    
-    // <li>
-    //         <div class="lista-item-container">
-    //           <input type="checkbox" id="checkbox-1" />
-    //           <p>Ração de gato</p>
-    //         </div>
-    //         <p class="texto-data">Segunda-feira (31/10/2022) às 08:30</p>
-    // </li>
+});
 
-    console.log(li);
-}
+   
+
